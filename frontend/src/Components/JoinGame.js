c72baa3c55a44007888220155ead7de67c0323f2
stevo9510 <stevo9510@gmail.com>
2021-06-25 22:58:53 -0400

    Input validation for JoinGame

--- frontend/src/Components/JoinGame.js
+++ frontend/src/Components/JoinGame.js
@@ -1,29 +1,49 @@
 import OptionBorder from "./OptionBorder";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import React, { useState } from "react";
 
-function JoinGame() {
+const JoinGame = () => {
+    const [userName, setUserName] = useState("");
+    const [gameCode, setGameCode] = useState("");
+    const MAX_GAME_CODE_SIZE = 5;
+
+    const userNameChanged = (event) => {
+        setUserName(event.target.value);
+    };
+
+    const gameCodeChanged = (event) => {
+        var cleanedInput = event.target.value
+            .replace(/[^0-9a-z]/gi, "")
+            .toUpperCase();
+
+        if (cleanedInput.length > MAX_GAME_CODE_SIZE) {
+            event.preventDefault();
+        } else {
+            setGameCode(cleanedInput);
+        }
+    };
+
     return (
         <OptionBorder title="Join Existing Game">
-            <Form>
-                <Form.Group className="mb-3" controlId="formGameCode">
-                    <Form.Control
-                        type="text"
-                        placeholder="5 Letter Game Code"
-                    ></Form.Control>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formName">
-                    <Form.Control
-                        type="text"
-                        placeholder="User Name"
-                    ></Form.Control>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Play!
-                </Button>
-            </Form>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="5 Letter Game Code"
+                    value={gameCode}
+                    onChange={gameCodeChanged}
+                ></FormControl>
+            </InputGroup>
+            <InputGroup className="mb-3">
+                <FormControl
+                    placeholder="User Name"
+                    value={userName}
+                    onChange={userNameChanged}
+                ></FormControl>
+            </InputGroup>
+            <Button variant="primary">Play!</Button>
         </OptionBorder>
     );
-}
+};
 
 export default JoinGame;
